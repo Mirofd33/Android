61c44e6f4019c85652e7777f52ef90bfd4565c47
Mirofd33 <xunsizuobi84@126.com>
2021-05-13 22:35:17 +0800

    第一次作业

---
+++ src/main/java/com/example/wy/myapplication/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.wy.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
