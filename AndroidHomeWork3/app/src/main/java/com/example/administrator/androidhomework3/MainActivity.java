3242d623bd95a8d88c0a55542d36f8f133a97a28
Mirofd33 <xunsizuobi84@126.com>
2021-06-07 21:00:18 +0800

    完成了

--- AndroidHomeWork3/app/src/main/java/com/example/administrator/androidhomework3/MainActivity.java
+++ AndroidHomeWork3/app/src/main/java/com/example/administrator/androidhomework3/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
     EditText mEtHeight;//身高控件
     RadioGroup mRegSex;//性别控件
     Button mBtnCompute;//计算按钮控件
-
+    TextView mTvResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,9 +31,31 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
               public void onClick(View v){
-                //
+                //onclick方法是点击后将要调用的方法
+                String sHeight = mEtHeight.getText().toString();
+                int height = Integer.parseInt(sHeight);//将字符转换整数
 
-                
+
+                int height2 = Integer.parseInt(mEtHeight.getText().toString());
+
+                int id =mRegSex.getCheckedRadioButtonId();//获取选中的单选按钮id
+                if(id==R.id.rb_male){
+                    //男性的标准体重计算公式=（身高cm-100）*0.9(kg);
+                    int standarweight =(int)((height-100)*0.9);
+
+                    //显示标准体重
+                    mTvResult.setText("标准体重是："+standarweight+"KG");
+                }else if(id==R.id.rb_female){
+                    //是女性 女性的标准体重计算公式=（身高cm-100）*0.9(kg)-2.5kg
+                    int standarweight =(int)((height-100)*0.9-2.5);
+                    
+                    mTvResult.setText("标准体重是："+standarweight+"KG");
+
+
+
+                }
+
+
             }
         });
     }
